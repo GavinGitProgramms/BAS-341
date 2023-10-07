a869a80fcdc0af5724fb6a807d8626a569de9585
Isaiah Heilman <isaiahheilman@gmail.com>
2023-10-07 17:27:19 -0500

    chore(db): scaffold database shared library

---
+++ db/rollup.config.js
@@ -0,0 +1,29 @@
+import typescript from 'rollup-plugin-typescript2'
+import { nodeResolve } from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import { terser } from 'rollup-plugin-terser'
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      file: 'dist/bundle.esm.js',
+      format: 'esm',
+    },
+    {
+      file: 'dist/bundle.cjs.js',
+      format: 'cjs',
+    },
+    {
+      file: 'dist/bundle.umd.js',
+      name: 'bas-db',
+      format: 'umd',
+    },
+  ],
+  plugins: [
+    typescript(),
+    nodeResolve(),
+    commonjs(),
+    terser(), // Minify the output
+  ],
+}
