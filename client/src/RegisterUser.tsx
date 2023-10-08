75c00da9e0b3f7251865581afff27018da23926c
Gavin Holst <gavinholst@gmail.com>
2023-10-08 18:42:47 -0500

    chore: Add files via upload

---
+++ client/src/RegisterUser.tsx
@@ -0,0 +1,142 @@
+import { match } from 'assert'
+import { useRef, useState, useEffect } from 'react'
+
+const RegisterUser = () => {
+  const userRef: any = useRef()
+  const errRef: any = useRef()
+
+  const [user, setUser] = useState('')
+  const [validName, setValidName] = useState(false)
+  const [userFocus, setUserFocus] = useState(false)
+
+  const [pwd, setPwd] = useState('')
+  const [validPwd, setValidPwd] = useState(false)
+  const [pwdFocus, setPwdFocus] = useState(false)
+
+  const [matchPwd, setMatchPwd] = useState('')
+  const [validMatch, setValidMatch] = useState(false)
+  const [matchFocus, setMatchFocus] = useState(false)
+
+  const [errMsg, setErrMsg] = useState('')
+  const [success, setSuccess] = useState(false)
+
+  useEffect(() => {
+    userRef.current.focus()
+  }, [])
+
+  /* Need to add validation to make sure the username isn't already used (onSubmit)
+     Need to add username and password requirments  
+  */
+
+  useEffect(() => {
+    const result = true
+    setValidName(result)
+  }, [user])
+
+  useEffect(() => {
+    const result = true
+    setValidPwd(result)
+    const match = pwd === matchPwd
+    setValidMatch(match)
+  }, [pwd, matchPwd])
+
+  useEffect(() => {
+    setErrMsg('')
+  }, [user, pwd, matchPwd])
+
+  return (
+    <section>
+      <p
+        ref={errRef}
+        className={errMsg ? 'errmsg' : 'offscreen'}
+        aria-live="assertive"
+      >
+        {errMsg}
+      </p>
+      <h1>Register</h1>
+      <form>
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          ref={userRef}
+          autoComplete="off"
+          onChange={(e) => setUser(e.target.value)}
+          required
+          aria-invalid={validName ? 'false' : 'true'}
+          aria-describedby="uidnote"
+          onFocus={() => setUserFocus(true)}
+          onBlur={() => setUserFocus(false)}
+        />
+        <p
+          id="uidnote"
+          className={
+            userFocus && user && !validName ? 'instructions' : 'offscreen'
+          }
+        >
+          Username requirments here!!!
+        </p>
+
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          onChange={(e) => setPwd(e.target.value)}
+          required
+          aria-invalid={validPwd ? 'false' : 'true'}
+          aria-describedby="pwdnote"
+          onFocus={() => setPwdFocus(true)}
+          onBlur={() => setPwdFocus(false)}
+        ></input>
+        <p
+          id="pwdnote"
+          className={pwdFocus && !validPwd ? 'instructions' : 'offscreen'}
+        >
+          Password requirments here!!!
+        </p>
+
+        <label htmlFor="confirm_pwd">Confirm Password:</label>
+        <input
+          type="password"
+          id="confirm_pwd"
+          onChange={(e) => setMatchPwd(e.target.value)}
+          required
+          aria-invalid={validMatch ? 'false' : 'true'}
+          aria-describedby="confirmnote"
+          onFocus={() => setMatchFocus(true)}
+          onBlur={() => setMatchFocus(false)}
+        />
+        <p
+          id="confirmnote"
+          className={matchFocus && !validMatch ? 'instructions' : 'offscreen'}
+        >
+          Must match the frist password input field.
+        </p>
+
+        <button
+          disabled={!validName || !validPwd || !validMatch ? true : false}
+        >
+          Sign Up
+        </button>
+      </form>
+      <p>
+        Need a Service Provider Account?
+        <br />
+        <span className="line">
+          {/*put router here to registerservice*/}
+          <a href="#">Sign Up</a>
+        </span>
+      </p>
+      <p>
+        Already have an account?
+        <br />
+        <span className="line">
+          {/*put router here to sign in*/}
+          <a href="#">Sign In</a>
+        </span>
+      </p>
+    </section>
+  )
+}
+
+export default RegisterUser
