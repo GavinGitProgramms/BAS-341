b61a8e500ae4576c743cd84495662fbaf050886c
Will McCullough <mccullough7478@uwlax.edu>
2023-10-10 19:32:26 -0500

    chore(server): update session and cors info

--- server/src/server.ts
+++ server/src/server.ts
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3000
 app.use(
   cors({
     credentials: true,
-    origin: 'http://localhost:3001,http://localhost:3000',
+    origin: 'http://localhost:3001',
   }),
 )
 
