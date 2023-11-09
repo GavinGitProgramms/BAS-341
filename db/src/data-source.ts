6b98a94a3866e3ef078858b166e190a893336a28
Isaiah Heilman <isaiahheilman@gmail.com>
2023-11-09 11:05:06 -0600

    fix(db): include new migration in data source

--- db/src/data-source.ts
+++ db/src/data-source.ts
@@ -1,9 +1,12 @@
 import 'reflect-metadata'
 import { DataSource } from 'typeorm'
 import { Appointment, Qualification, User } from './entity'
-import { Migration1696986519515 } from './migration'
+import {
+  Migration1696986519515,
+  AddAppointmentCanceled1699546327331,
+} from './migration'
 
-const migrations = [Migration1696986519515]
+const migrations = [Migration1696986519515, AddAppointmentCanceled1699546327331]
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
